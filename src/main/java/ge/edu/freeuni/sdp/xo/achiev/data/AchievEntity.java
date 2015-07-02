6c77d89bfec7b84a1635081b7bd4dc8043b4d7ef
ioane5 <ioane.sharvadze@gmail.com>
2015-07-03 00:38:02 +0400

    Implemented brute force get method.
    :-1:

--- src/main/java/ge/edu/freeuni/sdp/xo/achiev/data/AchievEntity.java
+++ src/main/java/ge/edu/freeuni/sdp/xo/achiev/data/AchievEntity.java
@@ -4,11 +4,16 @@ import com.microsoft.azure.storage.table.TableServiceEntity;
 
 public class AchievEntity extends TableServiceEntity{
 	private int score;
-
+	private int rank;
+	/**
+	 * We have only 1 partition. 
+	 */
+	public final static String PARTITION = "0"; 
+	
 	public AchievEntity(String ID,int score){
 		this.rowKey = ID;
 		final int index = 0;
-		this.partitionKey = ID.charAt(index)+"";
+		this.partitionKey = PARTITION; // same partition key for every user. 
 		this.score = score;
 	}
 	
@@ -19,4 +24,28 @@ public class AchievEntity extends TableServiceEntity{
 	public void setScore(int score) {
 		this.score = score;
 	}
+	
+	public int getRank(){
+		return rank;
+	}
+	
+	public void setRank(int rank){
+		this.rank = rank;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AchievEntity other = (AchievEntity) obj;
+		if (score != other.score)
+			return false;
+		return true;
+	}
+	
+	
 }
--- src/main/java/ge/edu/freeuni/sdp/xo/achiev/data/CloudRepository.java
+++ src/main/java/ge/edu/freeuni/sdp/xo/achiev/data/CloudRepository.java
@@ -1,12 +1,24 @@
 package ge.edu.freeuni.sdp.xo.achiev.data;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+import jersey.repackaged.com.google.common.collect.Lists;
+
 import com.microsoft.azure.storage.StorageException;
 import com.microsoft.azure.storage.table.CloudTable;
 import com.microsoft.azure.storage.table.TableOperation;
+import com.microsoft.azure.storage.table.TableQuery;
+import com.microsoft.azure.storage.table.TableQuery.QueryComparisons;
 
-public class CloudRepository implements Repository{
-	
+public class CloudRepository implements Repository {
+
 	private CloudTable table;
+
+	final String PARTITION_KEY = "PartitionKey";
+	final static String PARTITION = AchievEntity.PARTITION;
 
 	public CloudRepository(CloudTable table) {
 		this.table = table;
@@ -21,10 +33,28 @@ public class CloudRepository implements Repository{
 	@Override
 	public AchievEntity get(String id) throws Exception {
 		AchievEntity toGet = new AchievEntity(id, 0);
-		TableOperation query = TableOperation.retrieve(
-				toGet.getPartitionKey(), 
+
+		TableOperation query = TableOperation.retrieve(toGet.getPartitionKey(),
 				toGet.getRowKey(), AchievEntity.class);
-		return table.execute(query).getResultAsType();
+		toGet = table.execute(query).getResultAsType();
+		
+		String partitionFilter = TableQuery.generateFilterCondition(
+				PARTITION_KEY, QueryComparisons.EQUAL, PARTITION);
+
+		TableQuery<AchievEntity> partitionQuery = TableQuery.from(
+				AchievEntity.class).where(partitionFilter);
+
+		Iterable<AchievEntity> wholeTable = table.execute(partitionQuery);
+		ArrayList<AchievEntity> wholeTableList = Lists.newArrayList(wholeTable
+				.iterator());
+		Collections.sort(wholeTableList, new Comparator<AchievEntity>() {
+			@Override
+			public int compare(AchievEntity o1, AchievEntity o2) {
+				return Integer.compare(o1.getScore(), o2.getScore());
+			}
+		});
+		toGet.setRank(wholeTableList.indexOf(toGet));
+		return toGet;
 	}
 
 }
